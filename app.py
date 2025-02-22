4c76d6275de0e0a126bc26e1fbbbb33ba18f6a09
sudiptarathi2020 <rathisudipta99@gmail.com>
2025-02-22 18:15:59 +0600

    Added html in the templates folder

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-from flask import Flask, request, make_response
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 def index():
-    return "Hello World"
+    value = 'Disputant'
+    result = 20 + 40
+    mylist = [10,20,30,40]
+    return render_template('index.html',mylist=mylist)
 
-@app.route('/hello', methods=['GET','POST'])
-def hello():
-    response = make_response('Hello World')
-    response.status_code = 202
-    response.headers['content-type'] = 'application/json'
-    return response
-@app.route('/add/<int:number1>/<int:number2>')
-def add(number1, number2):
-    return f'{number1} + {number2} = {number1 + number2}'
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
 
-
-@app.route('/handle_url_params')
-def handle_url_params():
-    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
-        greeting = request.args['greeting']
-        name = request.args.get('name')
-        return f'{greeting}, {name}'
-    else:
-        return 'Some Parameters are missing'
-
-
-@app.route('/greet/<name>')
-def greet(name):
-    return f"Hello {name}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port = 5000, debug = True)
