db853ee51d14c7ff6a9298960a9d238d0144e2e1
sudiptarathi2020 <rathisudipta99@gmail.com>
2025-02-22 13:17:49 +0600

    updated requirements.txt

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def index():
+    return "Hello World"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port = 5000, debug = True)
